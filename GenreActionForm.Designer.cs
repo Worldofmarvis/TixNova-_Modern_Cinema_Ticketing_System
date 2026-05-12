7850677691ba3bed1344aeed8eb80513cb0da41d
Worldofmarvis <vistoedmar25@gmail.com>
2026-05-12 09:20:51 -0400

    Add files via upload

---
+++ GenreActionForm.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace TixNova__Final
+{
+    partial class GenreActionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GenreActionForm));
+            this.subheadingGradientLabel1 = new TixNova__Final.SubheadingGradientLabel();
+            this.roundedPictureBox1 = new TixNova_Final.RoundedPictureBox();
+            this.gradientLabel2 = new TixNova_Final.GradientLabel();
+            this.roundedPictureBox2 = new TixNova_Final.RoundedPictureBox();
+            this.gradientLabel1 = new TixNova_Final.GradientLabel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.roundedPictureBox3 = new TixNova_Final.RoundedPictureBox();
+            this.roundedPictureBox4 = new TixNova_Final.RoundedPictureBox();
+            this.roundedPictureBox5 = new TixNova_Final.RoundedPictureBox();
+            this.roundedPictureBox6 = new TixNova_Final.RoundedPictureBox();
+            this.roundedPictureBox7 = new TixNova_Final.RoundedPictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // subheadingGradientLabel1
+            // 
+            this.subheadingGradientLabel1.AutoSize = true;
+            this.subheadingGradientLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.subheadingGradientLabel1.Font = new System.Drawing.Font("Segoe UI Black", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subheadingGradientLabel1.Location = new System.Drawing.Point(720, 33);
+            this.subheadingGradientLabel1.Name = "subheadingGradientLabel1";
+            this.subheadingGradientLabel1.Size = new System.Drawing.Size(532, 65);
+            this.subheadingGradientLabel1.TabIndex = 36;
+            this.subheadingGradientLabel1.Text = "Action & Adventureses";
+            // 
+            // roundedPictureBox1
+            // 
+            this.roundedPictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.roundedPictureBox1.BorderRadius = 0;
+            this.roundedPictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.roundedPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("roundedPictureBox1.Image")));
+            this.roundedPictureBox1.Location = new System.Drawing.Point(-6, 11);
+            this.roundedPictureBox1.Name = "roundedPictureBox1";
+            this.roundedPictureBox1.Size = new System.Drawing.Size(203, 88);
+            this.roundedPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roundedPictureBox1.TabIndex = 37;
+            this.roundedPictureBox1.TabStop = false;
+            this.roundedPictureBox1.Click += new System.EventHandler(this.RoundedPictureBox1_Click);
+            // 
+            // gradientLabel2
+            // 
+            this.gradientLabel2.AutoSize = true;
+            this.gradientLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.gradientLabel2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradientLabel2.Location = new System.Drawing.Point(174, 33);
+            this.gradientLabel2.Name = "gradientLabel2";
+            this.gradientLabel2.Size = new System.Drawing.Size(203, 32);
+            this.gradientLabel2.TabIndex = 38;
+            this.gradientLabel2.Text = "Main Dashboard";
+            // 
+            // roundedPictureBox2
+            // 
+            this.roundedPictureBox2.BorderRadius = 20;
+            this.roundedPictureBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.roundedPictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("roundedPictureBox2.Image")));
+            this.roundedPictureBox2.Location = new System.Drawing.Point(348, 154);
+            this.roundedPictureBox2.Margin = new System.Windows.Forms.Padding(10, 3, 10, 3);
+            this.roundedPictureBox2.Name = "roundedPictureBox2";
+            this.roundedPictureBox2.Size = new System.Drawing.Size(293, 387);
+            this.roundedPictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roundedPictureBox2.TabIndex = 39;
+            this.roundedPictureBox2.TabStop = false;
+            // 
+            // gradientLabel1
+            // 
+            this.gradientLabel1.AutoSize = true;
+            this.gradientLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.gradientLabel1.Font = new System.Drawing.Font("Segoe UI Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradientLabel1.Location = new System.Drawing.Point(1635, 23);
+            this.gradientLabel1.Name = "gradientLabel1";
+            this.gradientLabel1.Size = new System.Drawing.Size(192, 45);
+            this.gradientLabel1.TabIndex = 40;
+            this.gradientLabel1.Text = "TIXNOVA+";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(1768, -4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(182, 116);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 41;
+            this.pictureBox4.TabStop = false;
+            // 
+            // roundedPictureBox3
+            // 
+            this.roundedPictureBox3.BorderRadius = 20;
+            this.roundedPictureBox3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.roundedPictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("roundedPictureBox3.Image")));
+            this.roundedPictureBox3.Location = new System.Drawing.Point(825, 154);
+            this.roundedPictureBox3.Margin = new System.Windows.Forms.Padding(10, 3, 10, 3);
+            this.roundedPictureBox3.Name = "roundedPictureBox3";
+            this.roundedPictureBox3.Size = new System.Drawing.Size(293, 387);
+            this.roundedPictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roundedPictureBox3.TabIndex = 42;
+            this.roundedPictureBox3.TabStop = false;
+            // 
+            // roundedPictureBox4
+            // 
+            this.roundedPictureBox4.BorderRadius = 20;
+            this.roundedPictureBox4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.roundedPictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("roundedPictureBox4.Image")));
+            this.roundedPictureBox4.Location = new System.Drawing.Point(1317, 154);
+            this.roundedPictureBox4.Margin = new System.Windows.Forms.Padding(10, 3, 10, 3);
+            this.roundedPictureBox4.Name = "roundedPictureBox4";
+            this.roundedPictureBox4.Size = new System.Drawing.Size(293, 387);
+            this.roundedPictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roundedPictureBox4.TabIndex = 43;
+            this.roundedPictureBox4.TabStop = false;
+            // 
+            // roundedPictureBox5
+            // 
+            this.roundedPictureBox5.BorderRadius = 20;
+            this.roundedPictureBox5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.roundedPictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("roundedPictureBox5.Image")));
+            this.roundedPictureBox5.Location = new System.Drawing.Point(348, 603);
+            this.roundedPictureBox5.Margin = new System.Windows.Forms.Padding(10, 3, 10, 3);
+            this.roundedPictureBox5.Name = "roundedPictureBox5";
+            this.roundedPictureBox5.Size = new System.Drawing.Size(293, 387);
+            this.roundedPictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roundedPictureBox5.TabIndex = 44;
+            this.roundedPictureBox5.TabStop = false;
+            // 
+            // roundedPictureBox6
+            // 
+            this.roundedPictureBox6.BorderRadius = 20;
+            this.roundedPictureBox6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.roundedPictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("roundedPictureBox6.Image")));
+            this.roundedPictureBox6.Location = new System.Drawing.Point(825, 603);
+            this.roundedPictureBox6.Margin = new System.Windows.Forms.Padding(10, 3, 10, 3);
+            this.roundedPictureBox6.Name = "roundedPictureBox6";
+            this.roundedPictureBox6.Size = new System.Drawing.Size(293, 387);
+            this.roundedPictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roundedPictureBox6.TabIndex = 45;
+            this.roundedPictureBox6.TabStop = false;
+            // 
+            // roundedPictureBox7
+            // 
+            this.roundedPictureBox7.BorderRadius = 20;
+            this.roundedPictureBox7.Cursor = System.Windows.Forms.Cursors.Default;
+            this.roundedPictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("roundedPictureBox7.Image")));
+            this.roundedPictureBox7.Location = new System.Drawing.Point(1317, 603);
+            this.roundedPictureBox7.Margin = new System.Windows.Forms.Padding(10, 3, 10, 3);
+            this.roundedPictureBox7.Name = "roundedPictureBox7";
+            this.roundedPictureBox7.Size = new System.Drawing.Size(293, 387);
+            this.roundedPictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roundedPictureBox7.TabIndex = 46;
+            this.roundedPictureBox7.TabStop = false;
+            // 
+            // GenreActionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1924, 1061);
+            this.Controls.Add(this.roundedPictureBox7);
+            this.Controls.Add(this.roundedPictureBox6);
+            this.Controls.Add(this.roundedPictureBox5);
+            this.Controls.Add(this.roundedPictureBox4);
+            this.Controls.Add(this.roundedPictureBox3);
+            this.Controls.Add(this.gradientLabel1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.roundedPictureBox2);
+            this.Controls.Add(this.gradientLabel2);
+            this.Controls.Add(this.roundedPictureBox1);
+            this.Controls.Add(this.subheadingGradientLabel1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GenreActionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GenreActionForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedPictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private SubheadingGradientLabel subheadingGradientLabel1;
+        private TixNova_Final.RoundedPictureBox roundedPictureBox1;
+        private TixNova_Final.GradientLabel gradientLabel2;
+        private TixNova_Final.RoundedPictureBox roundedPictureBox2;
+        private TixNova_Final.GradientLabel gradientLabel1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private TixNova_Final.RoundedPictureBox roundedPictureBox3;
+        private TixNova_Final.RoundedPictureBox roundedPictureBox4;
+        private TixNova_Final.RoundedPictureBox roundedPictureBox5;
+        private TixNova_Final.RoundedPictureBox roundedPictureBox6;
+        private TixNova_Final.RoundedPictureBox roundedPictureBox7;
+    }
+}
---
+++ GenreDrama.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TixNova__Final
+{
+    public partial class GenreDrama : Form
+    {
+        public GenreDrama()
+        {
+            InitializeComponent();
+        }
+
+        private void RoundedPictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RoundedPictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundedPictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundedPictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundedPictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GradientLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundedPictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GradientLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundedPictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SubheadingGradientLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
