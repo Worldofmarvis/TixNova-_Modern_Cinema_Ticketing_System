77c2deb16dbef1143940899fd3d261b57d57331d
Worldofmarvis <vistoedmar25@gmail.com>
2026-05-12 09:26:43 -0400

    Add files via upload

---
+++ RatedGForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TixNova__Final
+{
+    public partial class RatedGForm : Form
+    {
+        public RatedGForm()
+        {
+            InitializeComponent();
+        }
+
+        private void roundedPictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ RatedPG13.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TixNova__Final
+{
+    public partial class RatedPG13 : Form
+    {
+        public RatedPG13()
+        {
+            InitializeComponent();
+        }
+
+        private void roundedPictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
